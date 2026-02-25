2680d6a759ebe94961825d98ac1f85d322ac8d78
EduRS14 <eduardo14.05rivas@gmail.com>
2026-02-24 22:39:06 -0500

    feat: Agregado de resultados de partidos de la fecha 4 del Torneo Apertura 2026

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ import type { Partido } from '../types/partido';
 export default function App() {
 
     const [dataEquipos, setDataEquipos] = useState<Equipo[]>([]);
-    const [fechaActual, setFechaActual] = useState(4);
+    const [fechaActual, setFechaActual] = useState(5);
     const [listaPartidosAuxiliares, setListaPartidosAuxiliares] = useState<{ [key: string]: Partido[] }>({});
 
     const [sincronizando, setSincronizando] = useState(false);
