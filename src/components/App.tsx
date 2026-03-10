209ec997a3ec78240c59a75a7c996c23762b94aa
EduRS14 <eduardo14.05rivas@gmail.com>
2026-03-10 15:25:55 -0500

    feat: Fecha 7 predeterminada

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ import type { Partido } from '../types/partido';
 export default function App() {
 
     const [dataEquipos, setDataEquipos] = useState<Equipo[]>([]);
-    const [fechaActual, setFechaActual] = useState(5);
+    const [fechaActual, setFechaActual] = useState(7);
     const [listaPartidosAuxiliares, setListaPartidosAuxiliares] = useState<{ [key: string]: Partido[] }>({});
 
     const [sincronizando, setSincronizando] = useState(false);
